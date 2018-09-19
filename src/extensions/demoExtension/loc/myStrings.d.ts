b9f4ac90a0397142876ebaa602a046b5045a93da
Hima <36698844+heemas@users.noreply.github.com>
2018-09-19 16:29:22 +0530

    second commit

---
+++ src/extensions/demoExtension/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IDemoExtensionApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'DemoExtensionApplicationCustomizerStrings' {
+  const strings: IDemoExtensionApplicationCustomizerStrings;
+  export = strings;
+}
